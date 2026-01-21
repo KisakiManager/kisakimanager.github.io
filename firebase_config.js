9e29de5f3b92f8fb0eff040667763a30215083d9
KisakiManager <doggeeklabs@gmail.com>
2026-01-21 14:10:59 -0500

    Crear firebase_config.js

---
+++ firebase_config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBHabVfSJo3_xFqZUMCVvHsXtCJrS1NUP4",
+  authDomain: "kisaki-manager-85953.firebaseapp.com",
+  projectId: "kisaki-manager-85953",
+  storageBucket: "kisaki-manager-85953.firebasestorage.app",
+  messagingSenderId: "714700025635",
+  appId: "1:714700025635:web:9d526f9bb4e63531206696"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
